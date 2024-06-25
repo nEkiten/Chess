a9e59f6a0930fab697a08f4581cf9901837afcd5
Nikita Nikitin <nikita.nikitin0505@gmail.com>
2024-06-25 18:52:04 +0500

    Descrip library test

--- ChessLibrary/ChessLibrary/Class1.cs
+++ ChessLibrary/ChessLibrary/Class1.cs
@@ -1,7 +1,25 @@
 ﻿namespace ChessLibrary
 {
-    public class Class1
+    public class Chess
     {
+        string fen;
+
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
+            this.fen = fen;
+        }
+
+        public Chess Move(string move) //Будет содержать наименование двигаемой фигуры и ее начальное, конечное положение
+        {
+            Chess nextChess = new Chess(fen);
+            return nextChess;
+        }
+
+        public char GetFigureAt (int x, int y)
+        {
+            return '.';
+        }
+
 
     }
 }
